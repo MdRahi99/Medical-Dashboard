f70b957e3fa9870fa81640514752475f4adda19d
MdRahi99 <mdrahi181199@gmail.com>
2024-10-09 02:07:06 +0600

    modify

--- src/components/shared/DynamicTab.jsx
+++ src/components/shared/DynamicTab.jsx
@@ -6,24 +6,26 @@ const DynamicTab = ({ sections }) => {
   return (
     <div className="w-full space-y-8 md:space-y-12">
       {/* Tab Headers */}
-      <div className="flex items-center gap-6 overflow-auto no-scrollbar md:gap-12 border-b border-gray-200 mb-4 px-4">
-        {sections.map((section) => (
-          <button
-            key={section.name}
-            className={`px-4 py-2 font-roboto font-semibold transition-colors duration-300 ${
-              activeTab === section.name
-                ? 'border-b-2 border-[#2E68A3] text-[#2E68A3]'
-                : 'text-[#2c2c2c] hover:text-[#2E68A3] hover:border-b-2 border-[#2E68A3] transition-all duration-100 ease-in-out'
-            }`}
-            onClick={() => setActiveTab(section.name)}
-          >
-            {section.label}
-          </button>
-        ))}
+      <div className="sticky top-0 z-10 bg-white border-b border-gray-200 px-4">
+        <div className="flex items-center gap-6 overflow-auto no-scrollbar md:gap-12">
+          {sections.map((section) => (
+            <button
+              key={section.name}
+              className={`px-4 py-2 font-roboto font-semibold transition-colors duration-300 ${
+                activeTab === section.name
+                  ? 'border-b-2 border-[#2E68A3] text-[#2E68A3]'
+                  : 'text-[#2c2c2c] hover:text-[#2E68A3] hover:border-b-2 border-[#2E68A3] transition-all duration-100 ease-in-out'
+              }`}
+              onClick={() => setActiveTab(section.name)}
+            >
+              {section.label}
+            </button>
+          ))}
+        </div>
       </div>
 
       {/* Tab Content */}
-      <div className="">
+      <div>
         {sections.map(
           (section) =>
             activeTab === section.name && (
